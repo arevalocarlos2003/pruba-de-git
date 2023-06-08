05d3e7b2042762f2766ff82bd8c4da65158cef8e
GaboIraheta <00021223@uca.edu.sv>
2023-06-08 08:49:13 -0600

    nigga

--- main.cpp
+++ main.cpp
@@ -4,5 +4,10 @@ using namespace std;
 
 int main(){
   //cambio en rama carlos hecho por carlos
+  int array[10];
+  cout << "Ingrese datos \n";
+  for (int i=0; i<10; i++) {
+    cin >> array[i];
+  }
   return 0;
 }
