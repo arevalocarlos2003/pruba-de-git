903a295bf43855ef37284bcaa1e9574abff9e83f
arevalocarlos2003 <00010423@uca.edu.sv>
2023-06-09 15:22:11 -0600

    Se creo menu principal :sparkles:

--- Menu.hpp
+++ Menu.hpp
@@ -64,3 +64,20 @@ void MenuGen(){
   LimpiarPantalla();
   cout << "General Menu";
 }
+
+void MostrarCandidatos(){
+  cout << "candidato 1 [1]" << endl
+       << "candidato 2 [2]" << endl;
+
+  int opcion = 0;
+
+  switch (opcion){
+    case 1:
+      //candidato 1
+      //VotarCandidato1();
+      break;
+    case 2:
+      //candidato 2
+      break;
+  }
+}
