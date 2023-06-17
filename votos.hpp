ab3f16cebe16048814bdf05bbfc8ac3d6d1d5e08
arevalocarlos2003 <00010423@uca.edu.sv>
2023-06-16 18:22:25 -0600

    Se agregaron los comentarios :memo:

--- votos.hpp
+++ votos.hpp
@@ -7,11 +7,13 @@ using namespace std;
 struct Candidatos
 {
   int departamento;
+  //id sirve para luego reutlizarlo en la funcion de votar por tal candidato
   int id;
   string nombre;
   string apellido;
   string partido;
   int votos;
+  //se hace un array de esto
 } candidatos[56];
 
 void GuardarEnCandidatos()
@@ -19,6 +21,10 @@ void GuardarEnCandidatos()
   fstream Votos("votos.txt", ios::in);
   for (int i = 0; i < 56; i++)
   {
+    //Por cada vuelta del bucle este mismo leera una linea
+    //omitiendo espacios por lo tanto se usa el struct para captar lo importante
+    //Esto guarda cada una de las string y valores numericos en la estructura
+    //de esta manera podemos acceder a la estructura para cualquier cosa como escribir esos datos en el archivo
     Votos >> candidatos[i].departamento >> candidatos[i].id >> candidatos[i].nombre >> candidatos[i].apellido >> candidatos[i].votos >> candidatos[i].partido;
     cout << setfill('-')
          << "|"
@@ -40,7 +46,10 @@ void GuardarEnCandidatos()
 void ActualizarVotos(int idCandidato)
 {
   fstream Votos("votos.txt", ios::out);
+  //Se decrementa el numero ya que si se ingresa 1 ira a la linea 2
+  //porque el indice de los array empiezan en 0
   idCandidato--;
+  //recorre el array de structuras candidatos y guarda cada campo dejando un espacio para que se pueda volver a leer
   for (int i = 0; i < 56; i++)
   {
     if (i == idCandidato)
